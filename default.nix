ab8f77bf7267bcc1d14a4cfb7ee90176fbe1f825
Julien Debon <julien.debon@tweag.io>
2022-12-14 13:11:02 +0100

    Use custom default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,16 @@
-let
-  flake = (import
-    (
-      let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-      fetchTarball {
-        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-        sha256 = lock.nodes.flake-compat.locked.narHash;
-      }
-    )
-    { src = ./.; }
-  ).defaultNix;
-in
-flake.lib.${builtins.currentSystem} // flake.packages.${builtins.currentSystem}
+# let
+#   flake = (import
+#     (
+#       let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+#       fetchTarball {
+#         url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+#         sha256 = lock.nodes.flake-compat.locked.narHash;
+#       }
+#     )
+#     { src = ./.; }
+#   ).defaultNix;
+# in
+# flake.lib.${builtins.currentSystem} // flake.packages.${builtins.currentSystem}
+(import (fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz") {
+  src = ./.;
+}).defaultNix
